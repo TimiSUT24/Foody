44de9bb6d23ec69982375a747a3a9c89fbd8de4c
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-11-03 15:37:38 +0100

    Created models

---
+++ Backend/Domain/Models/Ingredient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public int FoodId { get; set; }
+
+        public int Number { get; set; }
+        public string Name { get; set; } = null!;
+        public decimal? WaterFactor { get; set; }
+        public decimal? FatFactor { get; set; }
+        public decimal? WeightBeforeCooking { get; set; }
+        public decimal? WeightAfterCooking { get; set; }
+        public decimal? CookingFactor { get; set; }
+
+        // Retention factors stored as JSON if complex
+        public string? RetentionFactorsJson { get; set; }
+
+        public Product? Food { get; set; }
+    }
+
+}
---
+++ Backend/Domain/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class User : IdentityUser
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+    }
+}
