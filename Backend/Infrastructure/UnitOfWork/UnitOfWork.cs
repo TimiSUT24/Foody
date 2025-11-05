42342b49c851dac176d084a5447fc3f6846f2dba
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-11-05 10:54:08 +0100

    Add UnitOfWork file

---
+++ Backend/Infrastructure/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.UnitOfWork
+{
+    public sealed class UnitOfWork
+    {
+        private readonly FoodyDbContext _context;
+
+        public UnitOfWork(FoodyDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> SaveChangesAsync(CancellationToken ct = default)
+        {
+            return await _context.SaveChangesAsync(ct);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
