0b246d812d0b62b8934f7c61c32fc96d5445a737
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2025-11-25 16:37:56 +0100

    beginning with checkout page

--- Frontend/src/Pages/CartPage.jsx
+++ Frontend/src/Pages/CartPage.jsx
@@ -1,5 +1,6 @@
 import {useCart} from "../Context/CartContext"
 import api from "../Api/api"
+import "../CSS/CartPage.css"
 
 export default function CartPage(){
     const {cart, addToCart, removeFromCart, totalPrice} = useCart();
@@ -25,21 +26,24 @@ export default function CartPage(){
         <div className="cart-page">
             <h1>Varukorg</h1>
 
+            <div id="cart-container">
             {cart.length === 0 ? (
                 <p>Din varukorg är tom</p>
             ) : (
                 <>
                 {cart.map(item => (
                     <div key={item.id} className="cart-item">
-                        <h3>{item.name}</h3>
-                        <p>{item.price}</p>
+                   
+                        <img src={item.imageUrl} alt="" style={{width:50}}/>
+                        <p className="p2" style={{fontSize:13}}>{item.name}</p>
+                        <p style={{fontSize:13}}>{item.price}</p>
 
                         <div className="qty-controls">
-                            <button onClick ={() => removeFromCart(item.id)}>-</button>
+                            <button onClick ={() => removeFromCart(item.id)} style={{borderRadius:10,borderStyle:"solid",borderWidth:2}}>-</button>
                             <span>{item.qty}</span>
-                            <button onClick ={() => addToCart(item)}>+</button>
+                            <button onClick ={() => addToCart(item)} style={{borderRadius:10,borderStyle:"solid",borderWidth:2}}>+</button>
+                   
                         </div>
-
                     </div>
                 ))}
                 
@@ -47,7 +51,9 @@ export default function CartPage(){
 
                 <button className="checkout-btn" onClick={placeOrder}>Lägg Order</button>
                 </>
+                
             )}
+            </div>
         </div>
     )
 }
